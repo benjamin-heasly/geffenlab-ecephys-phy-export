a6689380192269e539a0980237d4d8ac1fc391d4
benjamin-heasly <benjamin.heasly@gmail.com>
2025-06-11 17:53:31 +0000

    Sketch from ad-hoc scripts.

---
+++ code/export-to-phy.py
@@ -0,0 +1,39 @@
+import spikeinterface as si
+from spikeinterface.exporters import export_to_phy
+
+# pattern to match block0_imec0
+# configureable /data
+
+# pattern to locate .json
+# pattern to locate bin_dir
+recording_json = "/data/ecephys/preprocessed/block0_imec0.ap_recording1.json"
+bin_dir = "/data/ecephys_1/ecephys/AS20_03112025_trainingSingle6Tone2024_Snk3.1_g0_imec0/"
+recording_preprocessed = si.load(recording_json, base_folder=bin_dir)
+print(recording_preprocessed)
+
+# pattern to locate .zarr
+postprocessed_dir = "/data/ecephys/postprocessed/block0_imec0.ap_recording1.zarr/"
+sorting_analyzer = si.load_sorting_analyzer(postprocessed_dir, recording=recording_preprocessed)
+print(sorting_analyzer)
+
+# pattern to locate curated dir
+curated_dir = "/data/ecephys/curated/block0_imec0.ap_recording1/"
+sorting_curated = si.load(curated_dir)
+curated_properties = sorting_curated.get_property_keys()
+for property_name in curated_properties:
+  property_values = sorting_curated.get_property(property_name)
+  sorting_analyzer.set_sorting_property(property_name, property_values)
+
+# configurable /results
+# results subdir like recording json?
+phy_dir = "/results/ecephys/phy/"
+export_to_phy(
+  sorting_analyzer=sorting_analyzer,
+  output_folder=phy_dir,
+  remove_if_exists=True,
+  compute_pc_features=False,
+  additional_properties=curated_properties
+)
+
+# create cluster_info.tsv
+# https://github.com/benjamin-heasly/kilosort3-docker/blob/main/phy/create_cluster_info.py
